ab2154eb1229437e0b2807beb9874b1f93a681aa
Tomé Bumba Gúvulo <rudymilson9@gmail.com>
2024-07-20 20:16:54 +0100

    actualizando e commitando

--- ReceitasAo/views.py
+++ ReceitasAo/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from utils.recipes.factory import make_recipe
 # Create your views here.
 
 def home(request):
-    return render(request,'Receita/pages/home.html', context={
-        'name': 'Tome guvulo'})
+    return render(request,'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+        })
 
 
 def recipe(request, id):
-    return render(request,'Receita/pages/recipes.html', context={
-        'name': 'Tome guvulo'})
+    return render(request,'recipes/pages/recipes.html', context={
+        'recipe': make_recipe(),
+        })
 
